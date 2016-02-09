87506d06097dc1172cf1d3935205ad51c5d108a9
dougtc1 <douglastorres.usb@gmail.com>
2016-02-09 12:22:04 -0430

    gramatica libre de contexto y correcciones

--- parser.py
+++ parser.py
@@ -150,8 +150,6 @@ class Relacional(Expresion):
         self.tipo = "relacional"
         self.valor = valor
 
-# Verifica si te gusta mas que las reglas esten desglosadas o juntas
-
 def p_E_numero(p):
     '''E : TkNum
          | E TkSuma E
@@ -160,7 +158,7 @@ def p_E_numero(p):
          | E TkDiv E
          | E TkMod E
          | TkMenos E'''
-    p[0] = Numero(p[1])
+    p[0] = Numero(p[0])
 
 def p_E_booleano(p):
     '''E : TkBool
@@ -169,7 +167,7 @@ def p_E_booleano(p):
          | E TkConjuncion E
          | E TkDisyuncion E
          | TkNegacion E'''
-    p[0] = Booleano(p[1])
+    p[0] = Booleano(p[0])
 
 def p_E_relacional(p):
     '''E : E TkIgual E
@@ -178,11 +176,13 @@ def p_E_relacional(p):
          | E TkMayorigual E
          | E TkMenor E
          | E TkMayor E'''
-    p[0] = Relacional(p[1])
+    p[0] = Relacional(p[0])
 
-
-# Betancourt, esta seria nuestra declaracion para la instruccion TkStore?
 """
+###
+### Betancourt, estas funcionalidades las tenemos que hacer pero para otra entrega, cierto?
+###
+
 name = {}
 
 def p_statement_assign(p):
@@ -194,9 +194,61 @@ def p_statement_assign(p):
 
 # Gramatica libre de contexto
 
-def p_E_start(p):
-    '''Start : TkCreate Dec TkExecute InstC_list TkEnd
+def p_E_Start(p):
+    '''Start : TkCreate Dec TkExecute InstC TkEnd
              | TkExecute InstC_list TkEnd'''
 
 def p_E_Dec(p):
-    '''Dec : Type
+    '''Dec : Type TkBot ID_list TkEnd
+           | Type TkBot ID_list Comportamiento TkEnd'''
+
+def p_E_Type(p):
+    '''Type : TkInt
+            | TkBool
+            | TkChar'''
+
+def p_E_ID_list(p):
+    '''ID_list : ID_list, ID_list
+               | TkIdent'''
+
+def p_E_Comportamiento(p):
+    '''Comportamiento : TkOn Condicion TkDospuntos InstRobot_list TkEnd
+                      | TkPass''' #Creo que hay que agregar una instruccion vacia (TkPass) para que esto tenga sentido
+
+def p_E_Condicion(p):
+    '''Condicion : TkActivation
+                 | TkDeactivation
+                 | TkBool
+                 | default''' #Debe ser Tkdefault o esto es algo cuyo comportamiento implementamos despues?
+
+def p_E_InstC(p):
+    '''InstC : TkActivate ID_list
+             | TkDeactivate ID_list
+             | TkAdvance ID_list
+             | TkIf TkBool TkDospuntos InstC TkEnd
+             | TkIf TkBool TkDospuntos InstC TkElse InstC TkEnd
+             | TkWhile TkBool TkDospuntos InstC TkEnd
+             | Start
+             | InstC InstC'''
+
+def p_E_InstRobot_list(p):
+    '''InstRobot_list : TkStore E TkPunto InstRobot_list
+                      | TkStore TkMe E TkPunto InstRobot_list
+                      | TkCollect TkPunto InstRobot_list
+                      | TkCollect TkAs ID_list TkPunto InstRobot_list
+                      | TkDrop E TkPunto InstRobot_list
+                      | TkDrop TkMe TkPunto InstRobot_list
+                      | TkUp TkPunto InstRobot_list
+                      | TkDown TkPunto InstRobot_list 
+                      | TkLeft TkPunto InstRobot_list
+                      | TkRight TkPunto InstRobot_list
+                      | TkUp E TkPunto InstRobot_list
+                      | TkDown E TkPunto InstRobot_list
+                      | TkLeft E TkPunto InstRobot_list
+                      | TkRight E TkPunto InstRobot_list
+                      | TkRead TkPunto InstRobot_list
+                      | TkRead TkAs ID_list TkPunto InstRobot_list
+                      | TkSend TkPunto InstRobot_list
+                      | TkReceive TkPunto InstRobot_list'''
+
+# Parece que faltan 5 palabras que estan en la lista tokens de lexBOT.py y no estan en las palabras reservadas/simbolos reconocidos
