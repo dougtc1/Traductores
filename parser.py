b0bcfd3c9fd1cbd3ad423afe8bfc1adc1123e6d7
Benjamin Amos <benjamin.oxi@gmail.com>
2016-02-09 18:18:48 -0430

    cualquier mierda

--- parser.py
+++ parser.py
@@ -1,7 +1,7 @@
 # Yacc example
 
 import ply.yacc as yacc
-
+from arboles import Nodo
 # Get the token map from the lexer.  This is required.
 from lexBOT import tokens
 
@@ -103,99 +103,122 @@ def p_lambda(p):
     'lambda :'
     pass
 
-def p_E_Start(p):
+def p_estructura_Start(p):
     '''Start : TkCreate Dec TkExecute InstC TkEnd
              | TkExecute InstC TkEnd'''
+    p[0] = Nodo("S", None, [Nodo(p[1]), Nodo(p[2]), Nodo(p[3]), Nodo(p[4]), ])
 
-def p_E_Dec(p):
-    'Dec : Type TkBot ID_list Comportamiento TkEnd'
+def p_estructura_Declaraciones(p):
+    ''''Declaraciones : Declaraciones Declaraciones
+                      | Type TkBot ID_list Comportamiento TkEnd'''
 
-def p_E_Type(p):
+def p_expresion_Type(p):
     '''Type : TkInt
             | TkBool
             | TkChar'''
 
-def p_E_ID_list(p):
-    '''ID_list : ID_list, ID_list
+def p_expresion_ID_list(p):
+    '''ID_list : ID_list TkComa ID_list
                | TkIdent'''
 
-def p_E_Comportamiento(p):
-    '''Comportamiento : TkOn Condicion TkDospuntos InstRobot_list TkEnd
-                      | Comportamiento Comportamiento
+def p_instruccion_Comportamiento(p):
+    '''Comportamiento : Comportamiento Comportamiento
+                      | TkOn Condicion TkDospuntos InstRobot TkEnd
                       | lambda'''
 
-def p_E_Condicion(p):
+def p_instruccion_Condicion(p):
     '''Condicion : TkActivation
                  | TkDeactivation
                  | TkBool
-                 | TkDefault''' #Debe ser Tkdefault o esto es algo cuyo comportamiento implementamos despues?
+                 | TkDefault'''
 
-def p_E_InstC(p):
+def p_instruccion_InstC(p):
     '''InstC : TkActivate ID_list
              | TkDeactivate ID_list
              | TkAdvance ID_list
-             | TkIf TkBool TkDospuntos InstC TkEnd
-             | TkIf TkBool TkDospuntos InstC TkElse InstC TkEnd
-             | TkWhile TkBool TkDospuntos InstC TkEnd
+             | TkIf ExprBooleana TkDospuntos InstC TkEnd
+             | TkIf ExprBooleana TkDospuntos InstC TkElse InstC TkEnd
+             | TkWhile ExprBooleana TkDospuntos InstC TkEnd
              | Start
              | InstC InstC'''
 
-def p_E_InstRobot_list(p):
-    '''InstRobot_list : InstRobot_list InstRobot_list
-                      | TkStore Expr TkPunto 
-                      | TkCollect TkPunto 
-                      | TkCollect TkAs ID_list TkPunto 
-                      | TkDrop E TkPunto 
-                      | Dir TkPunto
-                      | Dir Expr TkPunto
-                      | TkRead TkPunto 
-                      | TkRead TkAs ID_list TkPunto 
-                      | TkSend TkPunto 
-                      | TkReceive TkPunto'''
+def p_instruccion_InstRobot(p):
+    '''InstRobot : InstRobot InstRobot
+                 | TkStore Expr TkPunto 
+                 | TkCollect TkPunto 
+                 | TkCollect TkAs ID_list TkPunto 
+                 | TkDrop ExprAritmetica TkPunto 
+                 | Dir TkPunto
+                 | Dir ExprAritmetica TkPunto
+                 | TkRead TkPunto 
+                 | TkRead TkAs ID_list TkPunto 
+                 | TkSend TkPunto 
+                 | TkReceive TkPunto'''
 
-def p_E_Dir(p):
-    '''Dir : TkUp
-           | TkDown
-           | TkLeft
-           | TkRight'''
+def p_instruccion_Direccion(p):
+    '''Direccion : TkUp
+                 | TkDown
+                 | TkLeft
+                 | TkRight'''
 
-def p_E_Expr(p):
-    '''Expr : AritBinexpr
-            | AritUnexpr
-            | BoolBinexpr
-            | BoolUnexpr
-            | TkParabre Expr TkParcierra
-            | TkNum
-            | TkBool
-            | TkMe'''
+def p_expresion_Expr(p):
+    '''Expr : ExprAritmetica
+            | ExprBooleana
+            | ExprRelacional'''
 
-def p_E_AritBinexpr(p):
-    '''Binexpr : Expr TkSuma Expr
-               | Expr TkResta Expr
-               | Expr TkMult Expr
-               | Expr TkDiv Expr
-               | Expr TkMod Expr
-               | Expr TkMayor Expr
-               | Expr TkMayorigual Expr
-               | Expr TkMenor Expr
-               | Expr TkMenorigual Expr
-               | Expr TkIgual Expr
-               | Expr TkNoigual Expr'''
+def p_expresion_RangoRel(p):
+    '''RangoRel : ExprAritmetica
+                | ExprBooleana'''
 
-def p_E_BoolBinexpr(p):
-    '''BoolBinexpr : Expr TkConjuncion Expr
-                   | Expr TkDisyuncion Expr
-                   | Expr TkMayor Expr
-                   | Expr TkMayorigual Expr
-                   | Expr TkMenor Expr
-                   | Expr TkMenorigual Expr
-                   | Expr TkIgual Expr
-                   | Expr TkNoigual Expr'''
+def p_expresion_ExprAritmetica(p):
+    '''ExprAritmetica : E TkSuma E
+                      | E TkResta E
+                      | E TkMult E
+                      | E TkDiv E
+                      | E TkMod E
+                      | E'''
 
-def p_E_AritUnexpr(p):
-    'Unexpr : TkResta Expr %prec TkMenos'
+def p_expresion_AritParentizada(p):
+    'E : (ExprAritmetica)'
 
-def p_E_BoolUnexpr(p):
-    'BoolUnexpr : TkNegacion Expr'
-    
+def p_expresion_AritVariable(p):
+    'E : TkIdent'
+
+def p_expresion_Numerica(p):
+    'E : TkNum'
+
+def p_expresion_Negativa(p):
+    'E : TkResta E %prec TkMenos'
+
+def p_expresion_EtoArit(p):
+    'E : ExprAritmetica'
+
+def p_expresion_ExprBooleana(p):
+    '''ExprBooleana : B TkConjuncion B
+                    | B TkDisyuncion B
+                    | B'''
+
+def p_expresion_BoolParentizada(p):
+    'B : (ExprBooleana)'
+
+def p_expresion_BoolVariable(p):
+    'B : TkIdent'
+
+def p_expresion_Booleana(p):
+    'B : TkBool'
+
+def p_expresion_Negada(p):
+    'B : TkNegacion B'
+
+def p_expresion_BtoBool(p):
+    'B : ExprBooleana'
+
+def p_expresion_Relacional(p):
+    '''ExprRelacional : RangoRel TkMayor RangoRel
+                      | RangoRel TkMayorigual RangoRel
+                      | RangoRel TkMenor RangoRel
+                      | RangoRel TkMenorigual RangoRel
+                      | RangoRel TkIgual RangoRel
+                      | RangoRel TkNoigual RangoRel'''   ## DOUGLAS ME ILUMINE NOJODAAAAAAAAA ##################
+
 # Parece que faltan 5 palabras que estan en la lista tokens de lexBOT.py y no estan en las palabras reservadas/simbolos reconocidos
